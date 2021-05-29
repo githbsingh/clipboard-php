ebbfbc57800b02a379fcefe9c1ad361a6869fab4
Mahendra Oraon <a4y0dzz@mmm.com>
2021-05-29 09:12:19 +0530

    changing db host

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 // Database configuration
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'ranchinow.com');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'clipboard');
