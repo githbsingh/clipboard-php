ebc648a00780e197d555e4f28b881f0e063ea18f
Mahendra Oraon <a4y0dzz@mmm.com>
2021-05-31 13:42:16 +0530

    gallery update

--- twitter-login/config.php
+++ twitter-login/config.php
@@ -2,19 +2,7 @@
 /* 
  * Basic Site Settings and API Configuration 
  */ 
- 
-// Database configuration 
-define('DB_HOST', 'localhost'); 
-define('DB_USERNAME', 'root'); 
-define('DB_PASSWORD', ''); 
-define('DB_NAME', 'clipboard'); 
-define('DB_USER_TBL', 'users'); 
- 
-// Twitter API configuration 
-define('TW_CONSUMER_KEY', 'qmkVKcZsx64qs2jLocadXa9b4'); 
-define('TW_CONSUMER_SECRET', '3MXlFzsLosOitwUzujupoDyjMEZnRei59n7hJVupqHCB4y6b88'); 
-define('TW_REDIRECT_URL', 'http://localhost/clipboard/twitter-login/'); 
- 
+require_once "../config.php";
 // Start session 
 if(!session_id()){ 
     session_start(); 
